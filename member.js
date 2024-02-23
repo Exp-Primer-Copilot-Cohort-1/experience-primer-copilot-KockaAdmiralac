44f10fc42d7d43e0b7afcf312b46aa7eb77536fc
Luka Simić <git@kocka.tech>
2024-02-23 20:37:41 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ['HTML', 'CSS', 'JS', 'React', 'Node', 'Python', 'Ruby', 'SQL'];
+    var member = {
+        name: 'John',
+        age: 25,
+        skills: skills
+    };
+    return member;
+}
